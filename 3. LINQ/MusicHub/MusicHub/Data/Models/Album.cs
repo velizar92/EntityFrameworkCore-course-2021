11f5d45fdcb438fe045d94e0afd04a80cc11c1ba
zako <velizar9209@gmail.com>
2021-11-14 12:37:10 +0200

    Uploads First Task from LINQ exercise

---
+++ 3. LINQ/MusicHub/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+
+        public Album()
+        {
+            this.Songs = new HashSet<Song>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price
+        {
+            get
+            {
+                return Songs.Sum(s => s.Price);
+            }
+        }
+
+        [ForeignKey(nameof(Producer))]
+        public int? ProducerId { get; set; }
+        public Producer Producer { get; set; }
+     
+        public virtual ICollection<Song> Songs { get; set; }
+
+    }
+}
