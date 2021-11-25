5b362b3793d1de401105e1905bbea88433e08690
zako <velizar9209@gmail.com>
2021-11-25 17:43:43 +0200

    Add 75% from EF Core exam (from 04.04.21)

---
+++ EF Core Exams/EF Core Exam 04.04.21/Data/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TeisterMask.Data.Models
+{
+    public class Employee
+    {
+
+        public Employee()
+        {
+            this.EmployeesTasks = new HashSet<EmployeeTask>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required]
+        public string Phone { get; set; }
+
+        public ICollection<EmployeeTask> EmployeesTasks { get; set; }
+    }
+}
---
+++ EF Core Exams/EF Core Exam 04.04.21/DataProcessor/Deserializer.cs
@@ -0,0 +1,89 @@
+﻿namespace TeisterMask.DataProcessor
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Newtonsoft.Json;
+    using TeisterMask.Data.Models;
+    using TeisterMask.DataProcessor.ImportDto;
+    using ValidationContext = System.ComponentModel.DataAnnotations.ValidationContext;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedProject
+            = "Successfully imported project - {0} with {1} tasks.";
+
+        private const string SuccessfullyImportedEmployee
+            = "Successfully imported employee - {0} with {1} tasks.";
+
+        public static string ImportProjects(TeisterMaskContext context, string xmlString)
+        {
+
+
+
+            return "";
+        }
+
+        public static string ImportEmployees(TeisterMaskContext context, string jsonString)
+        {
+            StringBuilder sb = new StringBuilder();
+            EmployeeInputModel[] employeeDtos = JsonConvert.DeserializeObject<EmployeeInputModel[]>(jsonString);
+            List<Employee> employees = new List<Employee>();
+
+            foreach (EmployeeInputModel employeeDto in employeeDtos)
+            {
+                if (!IsValid(employeeDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Employee e = new Employee()
+                {
+                    Username = employeeDto.Username,
+                    Email = employeeDto.Email,
+                    Phone = employeeDto.Phone                
+                };
+
+                foreach (int taskId in employeeDto.Tasks.Distinct())
+                {
+                    Task task = context.Tasks.Find(taskId);
+
+                    if (task == null)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    e.EmployeesTasks.Add(new EmployeeTask()
+                    {
+                        Task = task
+                    });
+                }
+
+                employees.Add(e);
+                sb.AppendLine(String.Format(SuccessfullyImportedEmployee, e.Username, e.EmployeesTasks.Count));
+            }
+
+            context.Employees.AddRange(employees);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
